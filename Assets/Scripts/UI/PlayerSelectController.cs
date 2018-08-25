b601f5b70fdfd44a5d14829c913ffa977d9fa928
OutokoneAlakerta <37738622+OutokoneAlakerta@users.noreply.github.com>
2018-08-25 22:42:35 +0300

    UI

---
+++ Assets/Scripts/UI/PlayerSelectController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelectController : MonoBehaviour {
+    public GameObject playerSelectionTab;
+    private List<PlayerSelectionTab> tabs;
+
+    // Use this for initialization
+    void OnEnable() {
+        if (!playerSelectionTab) Debug.Log("PlayerSelectionTab not set");
+        tabs = new List<PlayerSelectionTab>();
+    }
+
+
+    public void CreateNPlayers(int nPlayers) {
+        
+        for (int i = 0; i< nPlayers; ++i) {
+            GameObject obj = (GameObject)Instantiate(playerSelectionTab);
+            obj.transform.SetParent(transform);
+            obj.SetActive(true);
+            PlayerSelectionTab tab = obj.GetComponent<PlayerSelectionTab>();
+            tab.Initialize(i);
+            tabs.Add(tab);
+        }
+    }
+
+    public void SetLeftButton(int i, string name) {
+        tabs[i].SetLeftButton(name);
+    }
+
+    public void SetRightButton(int i, string name) {
+        tabs[i].SetRightButton(name);
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -13,12 +13,18 @@ public class UIManager : MonoBehaviour
 	private KeyCode[] keyBoard = new KeyCode[] { KeyCode.Backspace, KeyCode.Delete, KeyCode.Tab, KeyCode.Clear, KeyCode.Pause, KeyCode.Escape, KeyCode.Space, KeyCode.Keypad0, KeyCode.Keypad1, KeyCode.Keypad2, KeyCode.Keypad3, KeyCode.Keypad4, KeyCode.Keypad4, KeyCode.Keypad5, KeyCode.Keypad6, KeyCode.Keypad7, KeyCode.Keypad8, KeyCode.Keypad9, KeyCode.KeypadPeriod, KeyCode.KeypadPeriod, KeyCode.KeypadDivide, KeyCode.KeypadMultiply, KeyCode.KeypadMinus, KeyCode.KeypadPlus, KeyCode.KeypadEnter, KeyCode.KeypadEnter, KeyCode.KeypadEquals, KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.DownArrow, KeyCode.Insert, KeyCode.Home, KeyCode.End, KeyCode.PageUp, KeyCode.PageDown, KeyCode.F1, KeyCode.F2, KeyCode.F3, KeyCode.F4, KeyCode.F5, KeyCode.F6, KeyCode.F7, KeyCode.F8, KeyCode.F9, KeyCode.F10, KeyCode.F11, KeyCode.F12, KeyCode.Alpha0, KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9, KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F, KeyCode.G, KeyCode.H, KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.M, KeyCode.N, KeyCode.O, KeyCode.P, KeyCode.Q, KeyCode.R, KeyCode.S, KeyCode.T, KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X, KeyCode.Y, KeyCode.Z };
 	private KeyCode[] keysInUse;
 	RectTransform topTextPos, midTextPos;
+    public GameObject playerSelectUI;
+    private PlayerSelectController playerSelectController;
 
 	void Start()
 	{
 		topTextPos = topText.GetComponent<RectTransform>();
 		midTextPos = midText.GetComponent<RectTransform>();
+        playerSelectUI.SetActive(false);
 	}
+
+
+
 	void FixedUpdate()
 	{
 		switch(uiSequence)
@@ -30,7 +36,11 @@ public class UIManager : MonoBehaviour
 				if (!buttonSelectActive)
 				{
 					buttonSelectActive = true;
-					StartCoroutine(setButtons());
+                    playerSelectUI.SetActive(true);
+                    playerSelectController = playerSelectUI.GetComponent<PlayerSelectController>();
+                    playerSelectController.CreateNPlayers(playerCount);
+
+                    StartCoroutine(setButtons());
 				}
 				break;
 		}
@@ -56,7 +66,7 @@ public class UIManager : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.Return))
 		{
 			players = new Player[playerCount];
-			keysInUse = new KeyCode[playerCount];
+			keysInUse = new KeyCode[playerCount*2];
 			uiSequence = "buttonSelect";
 		}
 	}
@@ -74,13 +84,15 @@ public class UIManager : MonoBehaviour
 				{
 					if (Input.GetKeyDown(item) && players[i].getLeftKey() == KeyCode.None && checkIfUsed(keysInUse, item) == false)
 					{
-						players[i].setLeftKey(item);
-						keysInUse[i] = item;
+                        playerSelectController.SetLeftButton(i, item.ToString());
+                        players[i].setLeftKey(item);
+						keysInUse[2*i] = item;
 					}
 					else if (Input.GetKeyDown(item) && players[i].getRightKey() == KeyCode.None && item != players[i].getLeftKey() && checkIfUsed(keysInUse, item) == false)
 					{
-						players[i].setRightKey(item);
-						keysInUse[i] = item;
+                        playerSelectController.SetRightButton(i, item.ToString());
+                        players[i].setRightKey(item);
+						keysInUse[2*i+1] = item;
 					}
 				}
 
