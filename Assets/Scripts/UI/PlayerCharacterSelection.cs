3a26480b3259536d0fb0d3d34a81ddd103c2acb7
OutokoneAlakerta <37738622+OutokoneAlakerta@users.noreply.github.com>
2018-08-26 14:25:42 +0300

    UI code simplification and comments

--- Assets/Scripts/UI/PlayerCharacterSelection.cs
+++ Assets/Scripts/UI/PlayerCharacterSelection.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+//Handles players' key binding selections and also which rider and mount are chosen.
+//When timer goes to zero, go to next UISEQUENCE. Escape key goes back to previous sequence
 public class PlayerCharacterSelection : MonoBehaviour {
     private List<Player> players;
     private int playerCount = 0;
     private KeyCode[] keyBoard = new KeyCode[] { KeyCode.Backspace, KeyCode.Delete, KeyCode.Tab, KeyCode.Clear, KeyCode.Pause, KeyCode.Space, KeyCode.Keypad0, KeyCode.Keypad1, KeyCode.Keypad2, KeyCode.Keypad3, KeyCode.Keypad4, KeyCode.Keypad4, KeyCode.Keypad5, KeyCode.Keypad6, KeyCode.Keypad7, KeyCode.Keypad8, KeyCode.Keypad9, KeyCode.KeypadPeriod, KeyCode.KeypadPeriod, KeyCode.KeypadDivide, KeyCode.KeypadMultiply, KeyCode.KeypadMinus, KeyCode.KeypadPlus, KeyCode.KeypadEnter, KeyCode.KeypadEnter, KeyCode.KeypadEquals, KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.DownArrow, KeyCode.Insert, KeyCode.Home, KeyCode.End, KeyCode.PageUp, KeyCode.PageDown, KeyCode.F1, KeyCode.F2, KeyCode.F3, KeyCode.F4, KeyCode.F5, KeyCode.F6, KeyCode.F7, KeyCode.F8, KeyCode.F9, KeyCode.F10, KeyCode.F11, KeyCode.F12, KeyCode.Alpha0, KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9, KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F, KeyCode.G, KeyCode.H, KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.M, KeyCode.N, KeyCode.O, KeyCode.P, KeyCode.Q, KeyCode.R, KeyCode.S, KeyCode.T, KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X, KeyCode.Y, KeyCode.Z };
-    private KeyCode[] keysInUse;
+    private List<KeyCode> keysInUse;
     bool buttonSelectActive;
 
     [SerializeField]
@@ -24,6 +25,7 @@ public class PlayerCharacterSelection : MonoBehaviour {
     [SerializeField]
     private Text timerText;
 
+    //Find resource manager from the scene
     private void FindResourceManager() {
         if (!resourceManager)  resourceManager = (ResourceManager)FindObjectOfType(typeof(ResourceManager));
     }
@@ -40,12 +42,14 @@ public class PlayerCharacterSelection : MonoBehaviour {
         KillMe();
     }
 
+
+    //Use this to initialize the tabs
     public void Initialize(int _playerCount) {
         timer = countdown4start;
         playerCount = _playerCount;
         buttonSelectActive = false;
         players = new List<Player>();
-        keysInUse = new KeyCode[playerCount * 2];
+        keysInUse = new List<KeyCode>(playerCount*2);
         buttonSelectActive = false;
         FindResourceManager();
         playerSelectController.CreateNPlayers(playerCount, resourceManager);
@@ -54,13 +58,16 @@ public class PlayerCharacterSelection : MonoBehaviour {
         co = StartCoroutine(setButtons());
     }
 
+    //Destroy everything before calling initialize again
     private void KillMe() {
         playerSelectController.ClearMe();
         gameObject.SetActive(false);
+        keysInUse.Clear();
         StopCoroutine(co);
     }
 
 
+    //Decrement timer and control the sequence flow
     public UISEQUENCE UpdateMe(){
         if(Input.GetKeyDown(KeyCode.Escape)) {
             Debug.Log("ESCAPE");
@@ -78,8 +85,7 @@ public class PlayerCharacterSelection : MonoBehaviour {
         return UISEQUENCE.BUTTONSELECT;
     }
 
-
-
+    //Set resources for each player here. 
     public List<Player> GetPlayers() {
 
         for (int i = 0; i < players.Count; i++) {
@@ -91,8 +97,7 @@ public class PlayerCharacterSelection : MonoBehaviour {
         return players;
     }
 
-
-
+    //Set buttons for each player
     private IEnumerator setButtons() {
         for (int i = 0; i < playerCount; i++) {
             players.Add(new Player(i));
@@ -100,15 +105,18 @@ public class PlayerCharacterSelection : MonoBehaviour {
 
             while (true) {
                 foreach (var item in keyBoard) {
-                    if (Input.GetKeyDown(item) && players[i].getLeftKey() == KeyCode.None && checkIfUsed(keysInUse, item) == false) {
+                    if (!Input.GetKeyDown(item)) continue;
+                    if (keysInUse.Contains(item)) continue;
+                    
+                    if (players[i].getLeftKey() == KeyCode.None) {
                         playerSelectController.SetLeftButton(i, item);
                         players[i].setLeftKey(item);
-                        keysInUse[2 * i] = item;
+                        keysInUse.Add(item);
                         playerSelectController.SetTextRight(i, "S. Right Button");
-                    } else if (Input.GetKeyDown(item) && players[i].getRightKey() == KeyCode.None && item != players[i].getLeftKey() && checkIfUsed(keysInUse, item) == false) {
+                    } else if (players[i].getRightKey() == KeyCode.None) {
                         playerSelectController.SetRightButton(i, item);
                         players[i].setRightKey(item);
-                        keysInUse[2 * i + 1] = item;
+                        keysInUse.Add(item);
                     }
                 }
 
@@ -124,6 +132,8 @@ public class PlayerCharacterSelection : MonoBehaviour {
     }
 
    
+    //Check if the key binding is already taken 
+    //obsolete
     private bool checkIfUsed(KeyCode[] items, KeyCode i) {
         foreach (var item in items) {
             if (item == i) {
